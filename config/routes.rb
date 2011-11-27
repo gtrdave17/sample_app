319202702af11f6e2e3401858f91689d899cddd8
Dave Brown <dave.brown@bkwld.com>
2011-11-27 13:15:06 -0800

    removed index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/help"
-  get "pages/contact"
-  get "pages/about"
-  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+
+
 
 
   # The priority is based upon order of creation:
@@ -56,10 +57,11 @@ SampleApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   
-  #root :to => 'welcome#index'
+  root :to => 'pages#home'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
 end
